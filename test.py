2d10f130c63c2360e3f43050ee18e59a014b5da5
tejangupta <tejangupta8@gmail.com>
2023-08-13 09:09:37 +0530

    consumer script and database

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER', None)
     SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY', None)
     SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET', None)
+    MONGO_DB_URL = os.getenv('MONGO_DB_URL', default=None)
 
     print(API_KEY)
     print(ENDPOINT_SCHEMA_URL)
@@ -15,3 +16,4 @@ if __name__ == '__main__':
     print(BOOTSTRAP_SERVER)
     print(SCHEMA_REGISTRY_API_KEY)
     print(SCHEMA_REGISTRY_API_SECRET)
+    print(MONGO_DB_URL)
